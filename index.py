c1f16f93666d2000b56bb63fc48af5097fdc9464
mloves55k-coder <mloves55k@gmail.com>
2026-03-19 08:33:27 +0500

    Update and rename main.py to index.py

--- index.py
+++ index.py
@@ -5,19 +5,17 @@ import requests
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/extract')
+@app.route('/api/extract')
 def extract():
     url = request.args.get('url')
     if not url:
         return jsonify({"error": "No URL provided"}), 400
     try:
-        # Stable Third-party worker
         api_url = f"https://terabox-dl.qtcloud.workers.dev/api/get-info?url={url}"
         r = requests.get(api_url, timeout=15)
-        return jsonify(r.json())
+        return r.json()
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-if __name__ == "__main__":
-    app.run()
-  
+# Vercel handling
+app = app
